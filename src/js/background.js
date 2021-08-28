e4ac52436231efc0bd29e1ad57c7e83b0b8200f2
juhyoungRyu <thinkoutside2004@gmail.com>
2021-08-28 21:19:16 +0900

    210828

--- src/js/background.js
+++ src/js/background.js
@@ -1,32 +1,49 @@
 const box1 = document.querySelector("#box1");
 const box2 = document.querySelector("#box2");
-const circle = document.querySelector(".circle");
-const x1 = document.querySelector(".container h1:first-child");
-const x2 = document.querySelector(".container h1:last-child");
+const x1 = document.querySelector("#btn1");
+const x2 = document.querySelector("#btn2");
 
-
-
-function background() {
-    box1.style.transform = "scale(2.9)";
-    circle.classList.add("hidden");
-    x1.classList.remove("hidden");
+function background1() {
+    box1.style.width = "1000%";
+    box2.style.width = "40%";
+    x2
+        .classList
+        .remove("hidden");
 }
 
 function background2() {
-    box2.style.transform = "scale(2.9)";
-    circle.classList.add("hidden");
-    x2.classList.remove("hidden");
+    box2.style.width = "1000%";
+    box1.style.width = "40%";
+    x1
+        .classList
+        .remove("hidden");
+
+};
+
+function returnBg1() {
+    box1.style.width = "50%"
+    box2.style.width = "50%"
+    x1
+        .classList
+        .add("hidden");
+    x2
+        .classList
+        .add("hidden");
 }
 
-function return1() {
-    box2.style.transform = "scale(0.25)";
+function returnBg2() {
+    box1.style.width = "50%"
+    box2.style.width = "50%"
+    x1
+        .classList
+        .add("hidden");
+    x2
+        .classList
+        .add("hidden");
 }
 
-function return2() {
-    box1.style.transform = "scale(0.25)";
-}
+x1.addEventListener("click", returnBg1);
+x2.addEventListener("click", returnBg2);
 
-box1.addEventListener("click", background);
+box1.addEventListener("click", background1);
 box2.addEventListener("click", background2);
-x1.addEventListener("click", return1);
-x2.addEventListener("click", return2);
